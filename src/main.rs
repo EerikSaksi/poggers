956f20e610d7601e61f1741a86d703fe2cd6c0b1
Eerik Saksi <saksi.eerik@gmail.com>
2021-08-21 19:33:42 +0100

    Adding matches

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use graphql_parser::query::{parse_query, Definition, OperationDefinition};
-
 fn main() {
-    let ast = parse_query::<&str>("query MyQuery { field1, field2 }");
-    if let Ok(tree) = ast{
-        let definition = &tree.definitions[0];
-        if let Definition::Operation(operation_definition) = definition {
-            if let OperationDefinition::Query(query) = operation_definition {
-                println!("{}", query.selection_set);
+    println!("{}", build_query());
+}
+
+fn build_root() -> String {
+    let ast = parse_query::<&str>("query MyQuery { exercises {id, bodyPart}}");
+    if let Ok(tree) = ast {
+        for definition in tree.definitions.iter(){
+            match definition {
+                Definition::Operation(operation_definition) => build_operation_definition(operation_definition),
+                Definition::Fragment(fragment_definition) => return String("Definition::Fragment not implemented yet")
 
             }
         }
+        if let Definition::Operation(operation_definition) = definition {
+            if let OperationDefinition::Query(query) = operation_definition {
+                let to_return = String::from("");
+                for item in query.selection_set.items.iter(){
+                    match item {
+                        Selection::Field()
+                    }
+                }
+                return to_return;
+            }
+        }
     }
+    String::from("uh oh")
+}
+
+fn build_operation_definition(operation_definition: &OperationDefinition<&str>){
+
+}
+
+
+fn build_query(query: ) -> &str{
 }
