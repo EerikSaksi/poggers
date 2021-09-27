6afe656fd57fed0140ce976a1b1de3a29bf17171
Eerik Saksi <saksi.eerik@gmail.com>
2021-09-27 21:49:14 +0100

    Working with petgraph now

--- src/build_schema/internal_schema_info.rs
+++ src/build_schema/internal_schema_info.rs
@@ -1,48 +1,52 @@
 use crate::build_schema::{convert_pg_to_gql, read_database};
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
+use petgraph::{data::Build, graph::DiGraph, visit::IntoNodeReferences};
+use std::collections::HashSet;
 
-enum GraphQLSelection<'a> {
-    Field {
-        field_name: &'a str,
-    },
-    Composite {
-        foreign_table_name: &'a str,
-        foreign_selection: Rc<Vec<&'a GraphQLSelection<'a>>>,
-    },
+struct GraphQLType<'a> {
+    fields: HashSet<&'a str>,
+    table_name: &'a str,
 }
 
 fn create() {
-    let mut table_to_selection: HashMap<&str, Rc<Vec<&GraphQLSelection>>> = HashMap::new();
+    let types: Vec<GraphQLType> = Vec::new();
+    let mut g: DiGraph<GraphQLType, ()> = DiGraph::new();
     for current_row in read_database::read_tables().unwrap().iter() {
         let table_name: &str = current_row.get("table_name");
-        let column_name: &str = current_row.get("column_name");
+        //let column_name: &str = current_row.get("column_name");
         //let nullable: &str = current_row.get("nullable");
         let data_type: &str = current_row.get("data_type");
         let foreign_table_name: Option<&str> = current_row.get("foreign_table_name");
         let graphql_type = convert_pg_to_gql(data_type);
 
-        if !table_to_selection.contains_key(table_name) {
-            table_to_selection.insert(table_name, Rc::new(Vec::new()));
-        }
-        let selections = table_to_selection.get_mut(table_name).unwrap();
-        match foreign_table_name {
-            Some(foreign_table_name) => {
-                table_to_selection
-                    .entry(foreign_table_name)
-                    .or_insert_with(|| Rc::new(Vec::new()));
+        //add this table as a node if no such node
+        let foreign_index_optional = g.node_indices().find(|i| g[*i].table_name == table_name);
+        let source_index = match foreign_index_optional {
+            Some(index) => index,
+            None => g.add_node(GraphQLType {
+                fields: HashSet::new(),
+                table_name,
+            }),
+        };
 
-                let foreign_selection = table_to_selection.get(foreign_table_name).unwrap();
+        if let Some(foreign_table_name) = foreign_table_name {
+            //unwrap as we inserted this node if it was missing right above
+            //we dont know about the foreign table
+            let foreign_index_optional = g
+                .node_indices()
+                .find(|i| g[*i].table_name == foreign_table_name);
 
-                let a = GraphQLSelection::Composite {
-                    foreign_table_name,
-                    foreign_selection: Rc::clone(foreign_selection),
-                };
-                selections.borrow_mut()
-            }
-            None => println!("nice"),
+            //either return the index we found or insert and return the index of that new item
+            let foreign_index = match foreign_index_optional {
+                Some(foreign_index) => foreign_index,
+                None => g.add_node(GraphQLType {
+                    fields: HashSet::new(),
+                    table_name: foreign_table_name,
+                }),
+            };
+            g.add_edge(source_index, foreign_index, ());
         }
+        g[*source_index];
+
+        //current graphql type is either an existing item in the vector, or a newly inserted item
     }
 }
