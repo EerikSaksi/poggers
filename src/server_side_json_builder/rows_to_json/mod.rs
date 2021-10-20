a6307684b6bfdf244e822d990fc5fefa041e3316
Eerik Saksi <saksi.eerik@gmail.com>
2021-10-20 18:10:42 +0100

    SQL table in writeup

--- src/server_side_json_builder/rows_to_json/mod.rs
+++ src/server_side_json_builder/rows_to_json/mod.rs
@@ -1,2 +1,18 @@
-//pub fn convert(query: &str, Vec<>) {
+use postgres::{Client, NoTls};
+pub struct TableQueryInfo {
+    num_selected_cols: u16,
+    num_primary_keys: u16,
+}
+
+
+//pub fn convert(query: &str, table_query_info: Vec<TableQueryInfo>) {
+//    let mut client =
+//        Client::connect("postgres://eerik:Postgrizzly@localhost:5432/rpgym", NoTls).unwrap();
+//
+//    let rows = client.query(query, &[]).unwrap();
+//    let mut i = 0;
+//    let mut to_return = String::new();
+//    for row in rows {
+//
+//    }
 //}
