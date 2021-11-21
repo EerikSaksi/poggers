f986757cc842b1b110d5449c4978166499c81a80
Eerik Saksi <saksi.eerik@gmail.com>
2021-11-21 19:56:35 +0000

    What if introspection took a pool instead?

--- src/main.rs
+++ src/main.rs
@@ -18,102 +18,46 @@ mod config {
         }
     }
 }
-
 mod models {
     use serde::{Deserialize, Serialize};
-    use tokio_pg_mapper_derive::PostgresMapper;
 
-    #[derive(Deserialize, PostgresMapper, Serialize)]
-    #[pg_mapper(table = "users")] // singular 'user' is a keyword..
-    pub struct User {
-        pub email: String,
-        pub first_name: String,
-        pub last_name: String,
-        pub username: String,
+    #[derive(Deserialize, Serialize, Debug)]
+    pub struct GraphlQuery {
+        pub data: String,
     }
 }
-
-mod errors {
-    use actix_web::{HttpResponse, ResponseError};
-    use deadpool_postgres::PoolError;
-    use derive_more::{Display, From};
-    use tokio_pg_mapper::Error as PGMError;
-    use tokio_postgres::error::Error as PGError;
-
-    #[derive(Display, From, Debug)]
-    pub enum MyError {
-        NotFound,
-        PGError(PGError),
-        PGMError(PGMError),
-        PoolError(PoolError),
-    }
-    impl std::error::Error for MyError {}
-
-    impl ResponseError for MyError {
-        fn error_response(&self) -> HttpResponse {
-            match *self {
-                MyError::NotFound => HttpResponse::NotFound().finish(),
-                MyError::PoolError(ref err) => {
-                    HttpResponse::InternalServerError().body(err.to_string())
-                }
-                _ => HttpResponse::InternalServerError().finish(),
-            }
-        }
-    }
-}
-
-mod db {
-    use crate::{errors::MyError, models::User};
-    use deadpool_postgres::Client;
-    use tokio_pg_mapper::FromTokioPostgresRow;
-
-    pub async fn add_user(client: &Client) -> Result<User, MyError> {
-        let stmt = "select * from site_user";
-        client
-            .query(stmt, &[])
-            .await?
-            .iter()
-            .map(|row| User::from_row_ref(row).unwrap())
-            .collect::<Vec<User>>()
-            .pop()
-            .ok_or(MyError::NotFound) // more applicable for SELECTs
-    }
-}
-
 mod handlers {
-    use crate::{db, errors::MyError};
+    use crate::{models::GraphlQuery, server_side_json_builder::ServerSidePoggers};
     use actix_web::{web, Error, HttpResponse};
     use deadpool_postgres::{Client, Pool};
-
-    pub async fn add_user(db_pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
-        let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
-
-        let new_user = db::add_user(&client).await?;
-
-        Ok(HttpResponse::Ok().json(new_user))
+    pub async fn poggers(
+        query: web::Json<GraphlQuery>,
+        poggers: web::Json<ServerSidePoggers>,
+        db_pool: web::Data<Pool>,
+    ) -> Result<HttpResponse, Error> {
+        let query_info: GraphlQuery = query.into_inner();
+        println!("{}", query_info.data);
+        Ok(HttpResponse::Ok().json("{\"bigFloppa\": \"cute\"}"))
     }
 }
 
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
-use handlers::add_user;
+use handlers::poggers;
 use tokio_postgres::NoTls;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-
     let config = crate::config::Config::from_env().unwrap();
     let pool = config.pg.create_pool(NoTls).unwrap();
-
     let server = HttpServer::new(move || {
         App::new()
             .data(pool.clone())
-            .service(web::resource("/users").route(web::post().to(add_user)))
+            .service(web::resource("/graphql").route(web::post().to(poggers)))
     })
     .bind(config.server_addr.clone())?
     .run();
     println!("Server running at http://{}/", config.server_addr);
-
     server.await
 }
