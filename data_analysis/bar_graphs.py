24bd64ccc88f64f3621279ac518acb341257b4e3
Eerik Saksi <saksi.eerik@gmail.com>
2021-12-23 19:59:25 +0000

    Adding data analysis

---
+++ data_analysis/bar_graphs.py
@@ -0,0 +1,15 @@
+import json
+ 
+# Opening JSON file
+f = open('benchmark.json')
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+queries = [['tracks_media_some', 500], ['albums_tracks_genre_some', 500], ['albums_tracks_genre_all', 200]]
+
+
+def 
+for i in range(1,6):
+    print(timeparse(data['tracks_media_some on poggers'][str(i * 500)] ))
+
