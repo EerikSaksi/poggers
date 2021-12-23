94a79677db51c4676a4d6a3b97494080819d3516
Eerik Saksi <saksi.eerik@gmail.com>
2021-12-23 20:34:37 +0000

    Working now

--- data_analysis/bar_graphs.py
+++ data_analysis/bar_graphs.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 f = open('benchmark.json')
 
 data = json.load(f)
-queries = [['tracks_media_some', 500], [
-    'albums_tracks_genre_some', 500], ['albums_tracks_genre_all', 200]]
+queries = [['tracks_media_some', 500, 6], [
+    'albums_tracks_genre_some', 500, 6], ['albums_tracks_genre_all', 200, 5]]
 
 
 def duration_to_seconds(duration):
@@ -18,14 +18,18 @@ def duration_to_seconds(duration):
         return(float(duration[:-2]) * 60)
 
 
-for query in queries[0:1]:
-    plt.title('title name')
-    plt.xlabel('Requests per second')
+for query in queries:
+    plt.title(query[0])
+    plt.xlabel('Requests/second')
     plt.ylabel('Mean response latenency (seconds)')
     for tech in ['poggers', 'hasura', 'postgraphile']:
-        for i in range(1, 6):
-            rps = i * 500
-            seconds = duration_to_seconds(
-                data['tracks_media_some on ' + tech][str(rps)])
-            plt.plot(rps, seconds, label = tech)
-    plt.savefig("image.png")
+        x = []
+        y = []
+        for i in range(1, query[2]):
+            rps = query[1] * i
+            x.append(rps)
+            y.append(duration_to_seconds(
+                data[query[0] + ' on ' + tech][str(rps)]))
+        plt.plot(x, y, label=tech)
+        
+
