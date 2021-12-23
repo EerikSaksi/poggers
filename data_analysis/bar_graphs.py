a3ada088de7352821aadb38dc85138b2c2874070
Eerik Saksi <saksi.eerik@gmail.com>
2021-12-23 20:14:44 +0000

    Getting plotting working

--- data_analysis/bar_graphs.py
+++ data_analysis/bar_graphs.py
@@ -1,15 +1,31 @@
 import json
- 
-# Opening JSON file
+
+import matplotlib.pyplot as plt
+
 f = open('benchmark.json')
- 
-# returns JSON object as
-# a dictionary
+
 data = json.load(f)
-queries = [['tracks_media_some', 500], ['albums_tracks_genre_some', 500], ['albums_tracks_genre_all', 200]]
+queries = [['tracks_media_some', 500], [
+    'albums_tracks_genre_some', 500], ['albums_tracks_genre_all', 200]]
 
 
-def 
-for i in range(1,6):
-    print(timeparse(data['tracks_media_some on poggers'][str(i * 500)] ))
+def duration_to_seconds(duration):
+    if duration[-2:] == 'ms':
+        return(float(duration[:-2]) / 1000)
+    elif duration[-1] == 's':
+        return(float(duration[:-2]))
+    elif duration[-1] == 'm':
+        return(float(duration[:-2]) * 60)
 
+
+for query in queries[0:1]:
+    plt.title('title name')
+    plt.xlabel('Requests per second')
+    plt.ylabel('Mean response latenency (seconds)')
+    for tech in ['poggers', 'hasura', 'postgraphile']:
+        for i in range(1, 6):
+            rps = i * 500
+            seconds = duration_to_seconds(
+                data['tracks_media_some on ' + tech][str(rps)])
+            plt.plot(rps, seconds, label = tech)
+    plt.savefig("image.png")
